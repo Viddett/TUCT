bf8b8f49094dc955df0e5c6e5622e0ad7afa7a44
Elias Rhodén <elias.rhoden@hotmail.com>
2022-12-13 22:14:18 +0100

    start to work on blazor js interface

--- web/BlazorApp1/wwwroot/tree_comms.js
+++ web/BlazorApp1/wwwroot/tree_comms.js
@@ -8,13 +8,38 @@ function glenn_test(arg) {
 
 
 
-function get_tree_status(ip_adress) {
-
+async function get_tree_status(ip_adress) {
+    console.log("GET TREE STATUS")
+    var response = await fetch('http://' + ip_adress +'/state', {
+        method: 'GET',
+        headers: {
+            'Accept': 'application/json'
+        }//,
+        //body: JSON.stringify({ "id": 78912 })
+    })
+        .then(response => response.json())
+        //.then(response => console.log(JSON.stringify(response)))
+    //response_json = response.json()
+    //console.log(response_json)
+    return response
+    //yield response
 
 }
 
 function set_tree_status(ip_adress, obj) {
 
+    // JSON.stringify({ "id": 78912 })
+
+    fetch('http://' + ip_adress + '/', {
+        method: 'POST',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(obj)
+    })
+        .then(response => response.json())
+        .then(response => console.log(JSON.stringify(response)))
 
 }
 
