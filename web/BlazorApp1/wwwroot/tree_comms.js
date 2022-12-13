cc41f783d3ca7ecd1e3f4f7d3a07c778797c2c71
Elias Rhodén <elias.rhoden@hotmail.com>
2022-12-13 21:14:09 +0100

    Remoced files from gitignore

---
+++ web/BlazorApp1/wwwroot/tree_comms.js
@@ -0,0 +1,21 @@
+﻿
+
+function glenn_test(arg) {
+    alert("e du go eller denna e inte implementerad ännu");
+    console.log("glenn_tese")
+    console.log(arg)
+}
+
+
+
+function get_tree_status(ip_adress) {
+
+
+}
+
+function set_tree_status(ip_adress, obj) {
+
+
+}
+
+
