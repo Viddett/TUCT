70e1f692fab762cdc50745cb953880ae3f3d4475
Elias <elias.rhoden@hotmail.com>
2023-04-29 13:45:41 +0200

    Added tree comms over JS again

---
+++ web/BlazorApp1/wwwroot/tree_comms.js
@@ -0,0 +1,38 @@
+
+
+function glenn_test(arg) {
+    alert("e du go eller denna e inte implementerad ännu");
+    console.log("glenn_tese")
+    console.log(arg)
+}
+
+function get_tree_status(ip_adress) {
+    //console.log("GET TREE STATUS")
+
+    return fetch('http://' + ip_adress + '/state', {
+        method: 'GET',
+        headers: {
+            'Accept': 'application/json'
+        }
+    }).then(response => response.json())
+        .then(response => JSON.stringify(response) )
+}
+
+
+function set_tree_status(ip_adress, obj) {
+
+    // JSON.stringify({ "id": 78912 })
+
+    fetch('http://' + ip_adress + '/', {
+        method: 'POST',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(obj)
+    })
+        .then(response => response.json())
+        .then(response => console.log(JSON.stringify(response)))
+
+}
+
