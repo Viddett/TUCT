1b0ecb094123761c731aeb97473e895c3dfd63f4
Julius <lus.lus@live.se>
2024-01-07 12:50:26 +0100

    Updated server to host webpage
    Had to change streamreader and writer to be local variables.
    
    New endpoints to work with webpage calls.
    
    The JPG image doesn't want to be transfered due to memory issues.
    Perhaps it is possible to increase some memory block or allocation
    to make it happen or use a smaller sized image.

--- firmware/http_server.py
+++ firmware/http_server.py
@@ -45,19 +45,16 @@ async def connect_wifi(timeout_s:int=9):
 
 class HttpServer:
 
-    reader: asyncio.StreamReader
-    writer: asyncio.StreamWriter
-
     def __init__(self, get_state_callback, post_callback):
         self._get_callback = get_state_callback
         self._post_callback = post_callback
         self._stop_flag = False
 
-        self._html_response = index.html
+        with open("web-new/home.html") as file:
+            contents = file.readlines()
+            self._html_response = "\n".join(contents)
 
-    async def socket_handler(self, reader: asyncio.StreamReader, writer: asyncio.StreamWriter):
-        self.reader = reader
-        self.writer = writer
+    async def socket_handler(self,reader:asyncio.StreamReader,writer:asyncio.StreamWriter):
         addr = writer.get_extra_info('peername')
         print(f'Got a connection from {addr}.')
 
@@ -79,36 +76,64 @@ class HttpServer:
                 args[parts[0]] = parts[1]
 
         if method == 'OPTIONS':
-            await self.write_response(OK)
+            await self.write_response(writer,OK)
 
         elif method == 'GET':
-            await self._handle_get_2(url,args,request)
+            await self._handle_get_2(writer,url,args,request)
 
         elif method == 'POST':
-            await self._handle_post_2(url,args,request)
+            await self._handle_post_2(reader,writer,url,args,request)
 
         else:
-            await self.write_response(BAD_REQUEST,TEXT_HTML,index.html_bad_request)
+            await self.write_response(writer,BAD_REQUEST,TEXT_HTML,index.html_bad_request)
 
         print('Closing connection...')
         writer.close()
         await writer.wait_closed()
         print('Connection closed.')
 
-    async def _handle_get_2(self,url:str,args,request):
+    async def _handle_get_2(self,writer:asyncio.StreamWriter,url:str,args,request):
         if url == '/':
             # Default landing page
-            await self.write_response(OK,TEXT_HTML,self._html_response)
+            await self.write_response(writer,OK,TEXT_HTML,self._html_response)
         elif url == '/state':
             # return tree state as json
             state = self._get_callback()
             state_json = json.dumps(state)
-            await self.write_response(OK,APPLICATION_JSON,state_json)
+            await self.write_response(writer,OK,APPLICATION_JSON,state_json)
+        elif 'main.js' in url:
+            with open("web-new" + url) as file:
+                body = file.read()
+            content_type = 'text/javascript'
+            await self.write_response(writer,OK,content_type,body)
+        elif 'main.css' in url:
+            content_type = 'text/css'
+            with open("web-new" + url) as file:
+                body = file.read()
+            await self.write_response(writer,OK,content_type,body)
+        elif 'assets' in url:
+            full_url = url
+            print("**********" + full_url + "**********")
+            if '.svg' in url:
+                content_type = 'image/svg+xml'
+                with open(full_url, 'rb') as file:
+                    contents = file.read()
+            elif '.jpg' in url:
+                # Too big to transfer atm...
+                content_type = 'image/jpeg'
+                with open(full_url, 'rb') as file:
+                    contents = file.read()
+            await self.write_response(writer,OK,content_type,contents,True)
+        # elif '.mp3' in url:
+        #     content_type = 'audio/mpeg'
+        #     with open("web-new" + url, 'rb') as file:
+        #         contents = file.read()
+        #     await self.write_response(writer,OK,content_type,contents,True,True)
         else:
             # Bad request
-            await self.write_response(BAD_REQUEST,TEXT_HTML,index.html_bad_request)
+            await self.write_response(writer,BAD_REQUEST,TEXT_HTML,index.html_bad_request)
 
-    async def _handle_post_2(self,url,args,request:str):
+    async def _handle_post_2(self,reader:asyncio.StreamReader,writer:asyncio.StreamWriter,url,args,request:str):
         req_lines = request.split('\n')
         body = ""
         body_sep = '\r\n\r\n'
@@ -125,7 +150,7 @@ class HttpServer:
 
             extra_rec = body_len - len(body)
             if extra_rec > 0:
-                req_bytes: bytes = await self.reader.read(extra_rec)
+                req_bytes: bytes = await reader.read(extra_rec)
                 body += req_bytes.decode("utf-8")
 
         obj = {}
@@ -141,20 +166,21 @@ class HttpServer:
         except Exception as e:
             print("Failed to run post callback")
             print(e)
-            await self.write_response(BAD_REQUEST,TEXT_HTML,index.html_bad_request)
+            await self.write_response(writer,BAD_REQUEST,TEXT_HTML,index.html_bad_request)
             return
             # raise e
             # resp = {'status':'gud'}
 
-        await self.write_response(status,content_type,resp)
+        await self.write_response(writer,status,content_type,resp)
 
 
-    async def write_response(self,code:str,content_type:str='',body:str=''):
+    async def write_response(self,writer:asyncio.StreamWriter,code:str,content_type:str='',body:str='',binary=False,disposition=False):
         to_send = [f'HTTP/1.1 {code}']
         header = ["Access-Control-Allow-Origin: *",
                     "Access-Control-Allow-Credentials : true",
                     "Access-Control-Allow-Methods : GET,HEAD,OPTIONS,POST,PUT",
-                    "Access-Control-Allow-Headers:Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers"]
+                    "Access-Control-Allow-Headers:Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers",
+                    "X-Content-Type-Options : nosniff"]
 
         if code in [OK, CREATED]:
             to_send.extend(header)
@@ -162,15 +188,22 @@ class HttpServer:
         if content_type != '':
             to_send.append(f'Content-Type: {content_type}')
 
+        if disposition:
+            to_send.append(f'Content-Disposition: filename="NatKingCole-TheHappiestChristmasTree.mp3"')
+
         to_send.append('Connection: close')
 
-        if body != '':
+        if body != '' and not binary:
             to_send.append('')
             to_send.append(body)
 
         for message in to_send:
-            self.writer.write(message + '\n')
-            await self.writer.drain()
+            writer.write(message + '\n')
+            await writer.drain()
+
+        if binary:
+            writer.write(body)
+            await writer.drain()
 
 # Start of first solution
 # TODO Decide if remove
